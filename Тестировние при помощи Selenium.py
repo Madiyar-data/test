30693e93de3952d3f23ce4f96cc606be0c973138
Madiyar <90305690+Madiyar-data@users.noreply.github.com>
2022-03-09 19:05:37 +0600

    Add files via upload

---
+++ Тестировние при помощи Selenium.py
@@ -0,0 +1,19 @@
+
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Chrome(executable_path='C:\\Users\\Мадияр\\Desktop\\Test\\chromedriver.exe')
+
+#from selenium import webdriver
+
+driver.get("https://github.com/login")
+# найти поле имени пользователя / электронной почты и отправить само имя пользователя в поле ввода
+driver.find_element_by_id("login_field").send_keys('Madiyar-data')
+# найти поле ввода пароля и также вставить пароль
+driver.find_element_by_id("password").send_keys('Madik515568$')
+# нажмите кнопку входа в систему
+driver.find_element_by_name("commit").click()
+
+
